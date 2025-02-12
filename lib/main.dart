a053051b19e747225b159f881adb4afad4969aa6
Phi Le <phizzle2003@gmail.com>
2025-02-12 14:09:11 -0500

    Create timer

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -32,11 +33,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _time = 10;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+  // Use this to start the timer
+  void _startTimer() {
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (_time > 0) {
+        setState(() {
+          _time--;
+        });
+      } else {
+        timer.cancel();
+      }
     });
   }
 
@@ -59,20 +67,52 @@ class _MyHomePageState extends State<MyHomePage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               const Text(
-                'You have pushed the button this many times:',
+                'Happy Valentine\'s Day!',
+                style: TextStyle(fontSize: 30),
               ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
+              const SizedBox(height: 20),
+              AnimationTimer(time: _time),
             ],
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        // Temporary way to trigger timer
+        onPressed: _startTimer,
+        tooltip: 'Start Timer',
         child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class AnimationTimer extends StatelessWidget {
+  final int time;
+
+  const AnimationTimer({super.key, required this.time});
+
+ @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 243, 155, 155),
+        shape: BoxShape.rectangle,
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(color: Colors.blueGrey, width: 1),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Countdown Timer',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            '$time',
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+        ],
       ),
     );
   }
